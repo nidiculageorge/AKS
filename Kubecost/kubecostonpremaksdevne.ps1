ba99f6cf0873c4e04aa3dd92a134c4c5096732a4
Nithin Idicula George <43850474+nidiculageorge@users.noreply.github.com>
2024-05-12 15:27:55 +0530

    Update kubecostonpremaksdevne.ps1

--- Kubecost/kubecostonpremaksdevne.ps1
+++ Kubecost/kubecostonpremaksdevne.ps1
@@ -2,17 +2,20 @@
 
 helm uninstall kubecost-cloud -n kubecost-cloud
 
-#Installing Kube Cost from values-rm.yml
-
-helm install kubecost cost-analyzer --repo https://kubecost.github.io/cost-analyzer/ --namespace kubecost-ns --create-namespace --values values-rm.yml
-
-#Updating the license
-
-helm upgrade -i kubecost cost-analyzer --repo https://kubecost.github.io/cost-analyzer/ --namespace kubecost-ns --create-namespace --values values-rm.yml
-
 #AD Integration
 
 #https://docs.kubecost.com/install-and-configure/advanced-configuration/user-management-saml/microsoft-entra-id-saml-integration-for-kubecost
 
 kubectl create secret generic kubecost-azuread --from-file myservice.cert --namespace kubecost-ns
+
+#Installing Kube Cost from values-rm.yml
+
+helm install kubecost cost-analyzer --repo https://kubecost.github.io/cost-analyzer/ --namespace kubecost-ns --create-namespace --values values-rm.yml
+
+
+#Updating the license
+
+helm upgrade -i kubecost cost-analyzer --repo https://kubecost.github.io/cost-analyzer/ --namespace kubecost-ns --create-namespace --values values-rm.yml
+
+
 
